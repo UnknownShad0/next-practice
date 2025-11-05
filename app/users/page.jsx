a6b3ff1d5ab9f310f16a324c5d4150b427cbd14a
John Mark Anunciado <johnmark.anunciado@clearmindai.com>
2025-11-05 09:58:02 +0800

    Initial commit

---
+++ app/users/page.jsx
@@ -0,0 +1,22 @@
+async function getUsers() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    return res.json();
+  }
+  
+  export default async function Users() {
+    const users = await getUsers();
+  
+    return (
+      <div className="p-6">
+        <h1 className="text-xl font-bold">Users</h1>
+        <ul>
+          {users.map(user => (
+            <li key={user.id} className="py-2">
+              {user.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  
